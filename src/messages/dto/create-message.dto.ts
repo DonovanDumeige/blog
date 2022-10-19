c72b38223f304d33a1f9762d2116b651bab86226
DonovanDumeige <donovan.dumeige@gmail.com>
2022-10-19 15:05:56 +0200

    feat(dto): add validation fields for create message

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -1,8 +1,14 @@
 /* eslint-disable prettier/prettier */
-
+import { IsNotEmpty, Length, MaxLength} from "class-validator";
 // Le DTO sert de modèle pour l'envoie des données.
 // C'est un objet de transfert où l'on insère les données dont on a besoin.
 export class CreateMessageDto {
+  @IsNotEmpty()
+  @Length(2, 20)
+
   author: string;
+
+  @IsNotEmpty()
+  @MaxLength(500)
   content: string;
 }
