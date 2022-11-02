bb318d658403fdef119796015f87af8ec9e886b5
DonovanDumeige <donovan.dumeige@gmail.com>
2022-11-02 17:10:48 +0100

    chore: remove author field Author field is now autocompleted.

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -2,10 +2,6 @@ import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateMessageDTO {
   @IsString()
-  @IsOptional()
-  author: string;
-
-  @IsString()
   @IsNotEmpty()
   content: string;
 }
